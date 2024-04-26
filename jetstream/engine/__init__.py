b66fc8c70f16db9a7f14544ea440bf41cecd0227
Zhihao Shan <zhihaoshan@google.com>
2024-04-26 14:11:34 -0700

    fix lint

--- jetstream/engine/__init__.py
+++ jetstream/engine/__init__.py
@@ -12,18 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Initialization for any Engine implementation."""
+
 import jax
 
 
 def register_proxy_backend():
   """Try to register IFRT Proxy backend if it's needed."""
   # TODO: find a more elegant way to do it.
-  if jax.config.jax_platforms and 'proxy' in jax.config.jax_platforms:
+  if jax.config.jax_platforms and "proxy" in jax.config.jax_platforms:
     try:
       jax.lib.xla_bridge.get_backend("proxy")
     except RuntimeError:
       try:
-        from jaxlib.xla_extension import ifrt_proxy
+        from jaxlib.xla_extension import ifrt_proxy  # pylint: disable=import-outside-toplevel
+
         jax_backend_target = jax.config.read("jax_backend_target")
         jax._src.xla_bridge.register_backend_factory(  # pylint: disable=protected-access
             "proxy",
@@ -33,9 +36,10 @@ def register_proxy_backend():
             ),
             priority=-1,
         )
-        print(f"Registered IFRT Proxy backend with address {jax_backend_target}")
-      except Exception as e:
+        print(f"Registered IFRT Proxy with address {jax_backend_target}")
+      except ImportError as e:
         print(f"Failed to register IFRT Proxy, exception: {e}")
         pass
+
 
 register_proxy_backend()
