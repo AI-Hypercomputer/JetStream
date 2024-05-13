25d58c667fe112b3ee0950fc900b653a6633f3d1
Zijun Zhou <zijunzhou@google.com>
2024-05-13 20:32:45 +0000

    Update docstring

--- jetstream/core/utils/return_sample.py
+++ jetstream/core/utils/return_sample.py
@@ -10,10 +10,6 @@ import dataclasses
 class ReturnSample:
   """Both the token ids, their string representation, and other data.
 
-  Used so that the client knows when special token_ids have been returned
-  (i.e.<image_start>) and displays other modalities instead of text, but does
-  not burden the user with maintaining a vocab.
-
   Attributes:
     text: Text piece(s) detokenized from token id(s).
     token_ids: Raw result token id(s).
