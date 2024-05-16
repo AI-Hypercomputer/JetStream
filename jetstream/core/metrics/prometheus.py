d767b636735f004d28273e2ea4d1cf86f8b13571
Brendan Slabe <slabe@google.com>
2024-05-16 23:08:06 +0000

    __ -> _ in JetstreamMetricsCollector property names

--- jetstream/core/metrics/prometheus.py
+++ jetstream/core/metrics/prometheus.py
@@ -25,22 +25,22 @@ class JetstreamMetricsCollector:
     return cls.instance
 
   # Metric definitions
-  __prefill_backlog = Gauge(
+  _prefill_backlog = Gauge(
       name="jetstream_prefill_backlog_size",
       documentation="Size of prefill queue",
       labelnames=["hostname"],
   )
-  __slots_available_percentage = Gauge(
+  _slots_available_percentage = Gauge(
       name="jetstream_slots_available_percentage",
       documentation="The percentage of available slots in decode batch",
       labelnames=["hostname", "idx"],
   )
 
   def get_prefill_backlog_metric(self):
-    return _create_labeled_metric(self.__prefill_backlog, _MetricLabels())
+    return _create_labeled_metric(self._prefill_backlog, _MetricLabels())
 
   def get_slots_available_percentage_metric(self, idx: int):
     return _create_labeled_metric(
-        self.__slots_available_percentage,
+        self._slots_available_percentage,
         _MetricLabels(idx=idx),
     )
