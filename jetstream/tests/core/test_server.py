5d1a04a3515bc58ceae24167472599a5d1d87794
Brendan Slabe <slabe@google.com>
2024-05-17 20:27:26 +0000

    missing protocol scheme in url

--- jetstream/tests/core/test_server.py
+++ jetstream/tests/core/test_server.py
@@ -103,7 +103,7 @@ class ServerTest(unittest.IsolatedAsyncioTestCase):
         counter += 1
       server.stop()
 
-    async with requests.get(f"localhost:{metrics_port}") as response:
+    async with requests.get(f"http://localhost:{metrics_port}") as response:
       assert response.status_code == requests.status_codes.codes["ok"]
 
   def test_get_devices(self):
