d86b69befcb3897c471d5a2e8112b0605811e2d9
Brendan Slabe <slabe@google.com>
2024-12-02 21:33:21 +0000

    fmt

--- jetstream/core/server_lib.py
+++ jetstream/core/server_lib.py
@@ -119,7 +119,9 @@ def create_driver(
   shared_params = [ie.load_params() for ie in engines.interleaved_engines]
   logging.info("Loaded all weights.")
   if metrics_collector:
-    metrics_collector.get_model_load_time_metric().set(time.time() - model_load_start_time)
+    metrics_collector.get_model_load_time_metric().set(
+      time.time() - model_load_start_time
+    )
   interleaved_mode = (
       len(config.prefill_slices) + len(config.generate_slices) == 0
   )
