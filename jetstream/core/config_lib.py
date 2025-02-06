25979d20b2202fc31fc1fa56def19b0335942a2d
Yijia Jin <yijiaj@google.com>
2025-02-06 14:06:05 -0800

    Internal test
    PiperOrigin-RevId: 724067864

--- jetstream/core/config_lib.py
+++ jetstream/core/config_lib.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-# testtesttest
-
 """Configs of engines for the orchestrator to load."""
 
 import dataclasses
